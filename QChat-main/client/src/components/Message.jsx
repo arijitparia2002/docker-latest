0bcc3f6c869cf02ab4b7d3fa562537fcc963ea1e
Arijit Paria <arijitparia2020@gmail.com>
2024-01-28 16:40:40 +0530

    added docker configs for qChat ✅

--- QChat-main/client/src/components/Message.jsx
+++ QChat-main/client/src/components/Message.jsx
@@ -49,25 +49,25 @@ const Message = () => {
     return (
         <div className='flex flex-col w-2/3 h-screen my-1 justify-center'>
             <div className='items-center flex flex-col bg-gray-50 h-5/6 rounded-lg pb-2'>
-                <h3 className='text-2xl font-bold bg-blue-500 px-2 py-1 rounded rounded-b-none w-full text-center'>Messages</h3>
+                <h3 className='text-2xl font-bold bg-blue-500 px-2 py-1 rounded rounded-b-none w-full text-center'>QChat Messages</h3>
                 <div className='flex-grow p-4 overflow-y-auto w-full'>
                     {
 
                         messages.map((message, index) => {
                             return (
                                 <Typography variant="caption">
-                                <div key={index} className={` text-justify px-4 p-2 bg-white my-4 w-1/4 rounded-2xl text-lg ${message.isMine ? 'ml-auto rounded-br-none bg-blue-200 bg-opacity-50' : 'rounded-bl-none'
+                                <div key={index} className={` border border-solid text-justify px-4 p-2 shadow shadow-slate-400 my-4 w-1/4 rounded-2xl text-lg ${message.isMine ? 'ml-auto rounded-br-none bg-blue-200 bg-opacity-50 border-gray-400' : 'rounded-bl-none border-blue-400 '
                                     }`}>
                                     {message.msg}
                                 </div>
-                                </Typography>
+                                </Typography>  
                             )
                         }
                         )
                     }
                 </div>
                 <div className='flex flex-row w-full items-center px-2'>
-                    <input type="text" placeholder='Messege...' className='flex-grow p-4 bg-blue-100 rounded-l-lg' onChange={(event) => setMessage(event.target.value)} onKeyDown={handleKeyDown} value={message} />
+                    <input type="text" placeholder='Messege...' className='flex-grow p-4 bg-blue-100 rounded-l-lg' onChange={(event) => setMessage(event.target.value)} onKeyDown={handleKeyDown} value={message}/>
                     <button onClick={sendMessage} className=' ml-1 px-4 py-4 bg-blue-400 rounded-r-lg hover:bg-blue-500 font-bold'
                         disabled={!message}
                     >Send Messege</button>
